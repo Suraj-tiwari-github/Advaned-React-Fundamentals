538441908aa90b0398e1168cc57b63515786683a
PC <surajtiwari1155@gmail.com>
2024-05-06 17:18:54 +0530

    started with 03 conditional rendering

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,22 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
+
+  const[data,setData]=useState(null);
+
+  useEffect(()=>{
+    const fetchUser=async()=>{
+      try {
+        const res=await fetch(url);
+        const data=await res.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchUser();
+  },[])
+
   return <h2>Fetch Data </h2>;
 };
 export default MultipleReturnsFetchData;
