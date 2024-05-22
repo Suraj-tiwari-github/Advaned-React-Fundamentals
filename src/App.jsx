d9bac64e2dd75a5eb40f639bf80a19b5f4d07f6d
PC <surajtiwari1155@gmail.com>
2024-05-23 00:56:18 +0530

    Completed Form user challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import List from "./tutorial/05-leverage-javascript/starter/List.jsx";
+import List from "./tutorial/06-forms/starter/02-user-challenge";
 
 function App() {
   return (
     <div className="container">
-      <h2>Leveraging JavaScript</h2>
-      <List />
+      <List/>
     </div>
   );
 }
--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,65 @@
+import { useState } from "react";
+import { data } from "../../../data.js";
+
 const UserChallenge = () => {
+  const [name, setName] = useState("");
+  const [user, setUser] = useState(data);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submiited");
+    if(!name){
+      console.log("try submitting empty values")
+      return 
+    }
+    const newId = Date.now(); 
+    const newName = name;
+    // console.log(newId,newName)
+    const newUser = { id: newId, name: newName };
+    const updatedUser = [...user, newUser];
+    setUser(updatedUser);
+    setName('');
+  };
+
+  const removeUser=(id)=>{
+    const updatedUsers=user.filter((person)=>person.id!==id);
+    setUser(updatedUsers)
+  };
+
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
-      {/* render users below */}
+      <h2>Users</h2>
+      {
+        /* render users below */
+        user.map((person) => {
+          return <div>
+            <h4 key={person.id}>{person.name}</h4>
+          <button type='button' className='btn' onClick={()=>removeUser(person.id)}>Remove {person.name} </button>
+        </div>
+        })
+      }
     </div>
   );
 };
